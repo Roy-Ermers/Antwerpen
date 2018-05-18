33db2cfa43684251c6b4c8aee28bd605bcf3c62d
roy-ermers <dangergamesnl@gmail.com>
2018-05-18 14:29:11 +0200

    added flippable tiles

--- site/script.js
+++ site/script.js
@@ -1,13 +1,17 @@
 window.addEventListener('scroll', function () {
 	document.getElementsByTagName("nav")[0].classList.toggle("show", window.scrollY > 200);
+	document.getElementById("scrollbutton").style.opacity = window.scrollY < 200 ? 1 : 0;
 });
 
 function ScrollDown(target) {
-	window.scrollBy({
-		top: 600, // could be negative value
+	window.scrollTo({
+		top: 580, // could be negative value
 		left: 0,
 		behavior: 'smooth'
 	});
 }
-///////hooomoooooooooooooooooooooooooooooooooooooo, kan je even kijken of je de button iets hoger kan krijgen en je de button iet doorzichtiger kan maken
-//is goed 👌
+
+function Turn(elem) {
+	console.log(elem);
+	elem.closest("article").classList.toggle("switch");
+}
