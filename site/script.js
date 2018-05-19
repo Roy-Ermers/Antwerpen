eb7df97b9f201a2a9cb980bf46739922ab0d8e93
roy-ermers <dangergamesnl@gmail.com>
2018-05-19 20:15:13 +0200

    added advanced tiles

--- site/script.js
+++ site/script.js
@@ -1,24 +1,27 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', () => {
     document.getElementsByTagName("nav")[0].classList.toggle("show", window.scrollY > 200);
     document.getElementById("scrollbutton").style.opacity = window.scrollY < 200 ? 1 : 0;
 });
+var ActiveTile = undefined;
 document.querySelectorAll(".turn-on-hover").forEach((elem) => {
     elem.addEventListener("mouseenter", (ev) => {
+        if (ActiveTile != undefined) ActiveTile.closest("article").classList.toggle("switch", false);
         elem.closest("article").classList.toggle("switch", true);
-    })
-    elem.addEventListener("mouseleave", (ev) => {
-        elem.closest("article").classList.toggle("switch", false);
-    })
-})
+        ActiveTile = elem;
+    });
+});
 
 function ScrollDown(target) {
     window.scrollTo({
-        top: 590, // could be negative value
+        top: 590,
         left: 0,
-        behavior: 'smooth'
+        behavior: "smooth"
     });
 }
 
 function Turn(elem) {
+    if (ActiveTile != undefined && ActiveTile != elem.closest("article"))
+        ActiveTile.closest("article").classList.toggle("switch", false);
     elem.closest("article").classList.toggle("switch");
+    ActiveTile = elem.closest("article").classList.contains("switch") ? elem.closest("article") : undefined;
 }
